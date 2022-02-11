331a86147b2771195b721ee7b3f2e6fe01e98eb3
Achyuth Suchit <achyuthsuchit@gmail.com>
2022-02-11 11:38:56 +0530

    updated Workout3.html and Workout3.js

--- JS/Workout3.js
+++ JS/Workout3.js
@@ -1,4 +1,8 @@
 function handleSubmit() {
     let name = document.getElementById('t1').value;
-    alert("Hi, my name is " +name);
+    let pname = document.getElementById('t2').value;
+    let loc = document.getElementById('t3').value;
+    let desg = document.getElementById('t4').value;
+    let comp = document.getElementById('t5').value;
+    document.write("Hi! I'm " + name + " and my partner's name is " + pname + ". I stay in " + loc + " I am working as " + desg + " in " + comp);
 }// JavaScript source code
